3a9ca10af0633c36052940982ef3f36fc9597533
Daria Smirnova <darya.smirn1@yandex.ru>
2022-03-31 16:21:48 +0300

    Выполнила усложненное задание урока №6

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
 'use strict';
 
+const attempts = 10;
+
+let availableAttempts;
+
 let targetNumber;
 let userNumber;
+let isNewGame = true;
 
 const getRandomNumber = function (min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const playGame = function (userNumber) {
-	if (userNumber === null) {
+const startGame = function (isNewGame) {
+	if (isNewGame) {
+		availableAttempts = attempts;
+		targetNumber = getRandomNumber(1, 100);
+		userNumber = prompt('Угадай число от 1 до 100');
+		playGame(userNumber, availableAttempts);
+	} else {
 		alert('Игра окончена');
 		return;
 	}
+};
 
-	userNumber = +userNumber;
+const playGame = function (userNumber, attempts) {
+	if (attempts > 1) {
+		if (userNumber === null) {
+			alert('Игра окончена');
+			return;
+		}
+
+		userNumber = +userNumber;
 	
-	if (!isNumber(userNumber) || userNumber === 0) {
-		userNumber = prompt('Введите число!');
-		playGame(userNumber);
-	} else if(userNumber === targetNumber){
-		alert('Поздравляю, Вы угадали!!!');
-		return;
-	} else if (userNumber > targetNumber) {
-		userNumber = prompt('Загаданное число меньше! \nПопробуй ещё');
-		playGame(userNumber);
-	} else if (userNumber < targetNumber) {
-		userNumber = prompt('Загаданное число больше! \nПопробуй ещё');
-		playGame(userNumber);
+		if (!isNumber(userNumber) || userNumber === 0) {
+			attempts--;
+			userNumber = prompt('Введите число! Осталось ' + attempts + ' попыток');
+			playGame(userNumber, attempts);
+		} else if (userNumber === targetNumber) {
+			isNewGame = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');			
+			startGame(isNewGame);			
+		} else if (userNumber > targetNumber) {
+			attempts--;
+			userNumber = prompt('Загаданное число меньше! Осталось ' + attempts + ' попыток \nПопробуй ещё');
+			playGame(userNumber, attempts);
+		} else if (userNumber < targetNumber) {
+			attempts--;
+			userNumber = prompt('Загаданное число больше! Осталось ' + attempts + ' попыток \nПопробуй ещё');
+			playGame(userNumber, attempts);
+		}
+	} else {
+		isNewGame = confirm('Попытки закончились, хотите сыграть еще?');
+		startGame(isNewGame);
 	}
 };
 
@@ -34,6 +59,4 @@ const isNumber = function (num) {
 	return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-targetNumber = getRandomNumber(1, 100);
-userNumber = prompt('Угадай число от 1 до 100');
-playGame(userNumber);
+startGame(isNewGame);
